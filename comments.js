e9a6f09008de1be3e186aaa5d77cb47524779c88
Skipper_108 <36970853+LeonardoSforza@users.noreply.github.com>
2024-02-09 15:16:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,76 @@
-// Create a web server that listens on port 3000. It should respond to a GET request to /comments with an array of comments. Each comment should have an id, author, and message property. The id should be unique and increment by 1 for each new comment. Use the comments array below to get started.
+// Create web server and listen on port 3000
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsFile = path.join(__dirname, 'comments.json');
+var comments = [];
 
-const express = require('express');
-const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-const comments = [
-  { id: 1, author: 'John', message: 'Hello everyone' },
-  { id: 2, author: 'Jane', message: 'How is everyone doing?' },
-  { id: 3, author: 'Bob', message: 'I am doing well' }
-];
-
-app.get('/comments', (req, res) => {
-  res.json(comments);
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    comments = JSON.parse(data);
+    res.json(comments);
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.post('/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile(commentsFile, JSON.stringify(comments, null, 4), function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(comments);
+  });
 });
+
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
+```
+You can run the server with the following command:
+```
+$ node comments.js
+```
+The server will listen on port 3000. You can now use the following AJAX code to send the comments to the server:
+```javascript
+// Path: public/index.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Comment Box</title>
+  </head>
+  <body>
+    <div id="content"></div>
+    <form id="commentForm">
+      <input type="text" placeholder="Your name" name="author" />
+      <input type="text" placeholder="Say something..." name="text" />
+      <input type="submit" value="Post" />
+    </form>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script>
+      function getComments() {
+        $.ajax({
+          url: '/comments',
+          type: 'GET',
+          success: function(data) {
+            var html = '';
+            data.forEach(function(comment) {
+              html += '<div><strong>' + comment.author + '</strong>: ' + comment.text + '</div>';
+            });
+            $('#content').html(html);
+          }
+        });
+      }
+
+      function postComment(e) {
+        e.preventDefault();
+        var comment = {
